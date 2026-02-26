5204640dbca176e40ff8d9e3ba8caa9f93990d40
Israel Llanos <israel.llm@hotmail.com>
2026-02-26 15:54:42 -0500

    Creacion de la estructura basica del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CartAppComponent } from "./components/cart-app/cart-app.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet, CartAppComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'cart-app';
 }
