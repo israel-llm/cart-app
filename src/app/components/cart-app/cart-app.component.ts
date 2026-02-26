0f1d6ee89a8a1ef253de42a6681c009426be4b09
Israel Llanos <israel.llm@hotmail.com>
2026-02-26 17:52:55 -0500

    Creacion de subcomponentes para el catalogo de productos

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product';
+import { CatalogComponent } from '../catalog/catalog.component';
 
 @Component({
   selector: 'app-cart-app',
   standalone: true,
-  imports: [],
+  imports: [CatalogComponent],
   templateUrl: './cart-app.component.html'
 })
 export class CartAppComponent implements OnInit {
 
-  products: Product[] = [];
+  products!: Product[];
 
   constructor(private service: ProductService) {
   }
