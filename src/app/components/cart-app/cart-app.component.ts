e5a749e52fce7126a611c1405fd7149b1a8a07de
Israel Llanos <israel.llm@hotmail.com>
2026-03-04 21:23:45 -0500

    Guardando items en el sessionStorage

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -24,6 +24,7 @@ export class CartAppComponent implements OnInit {
 
   ngOnInit(): void {
     this.products = this.service.findAll();
+    this.items = JSON.parse(sessionStorage.getItem('cart')!) || [];
     this.calculateTotal();
   }
 
@@ -40,15 +41,20 @@ export class CartAppComponent implements OnInit {
       this.items = [...this.items, { product: { ...productAdd }, quantity: 1 }];
     }
     this.calculateTotal();
+    this.saveSession();
   }
 
   onDeleteCart(id: number): void {
     this.items = this.items.filter(item => item.product.id !== id);
     this.calculateTotal();
+    this.saveSession();
   }
 
-  calculateTotal(): void{
+  calculateTotal(): void {
     this.total = this.items.reduce((accumulator, item) => accumulator + item.quantity * item.product.price, 0);
   }
 
+  saveSession(): void {
+    sessionStorage.setItem('cart', JSON.stringify(this.items));
+  }
 }
